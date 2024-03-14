895bfabab1547b08729c082d8b203a69d54e476f
bathalasupraja <bathalasupraja2@gmail.com>
2024-03-14 18:37:58 +0530

    Initial Commit

---
+++ RegistrationFormApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationFormApp
+//
+//  Created by Supraja on 14/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
