951917c8a1e464e2b5c107c2572d861a130af2b6
bathalasupraja <bathalasupraja2@gmail.com>
2024-03-15 20:32:00 +0530

    Added TableViewCell  and  UIButton
    * Added UIButton in TableViewCell.
    * Added tableViewCell in ViewController.

--- RegistrationFormApp/RegistrationTableViewCell.swift
+++ RegistrationFormApp/RegistrationTableViewCell.swift
@@ -16,12 +16,24 @@ class RegistrationTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        nameTextField.delegate = self
+        nameTextField.tag = 100
         
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
     }
-
+}
+extension RegistrationTableViewCell: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.tag == 100 {
+            print("\n>>>> name text field")
+            nameTextField.becomeFirstResponder()
+        } else {
+            
+        }
+        return true
+    }
 }
